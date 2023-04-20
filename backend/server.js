36692cedb719502017b1084e7ef9225f4434a4bf
Miguel Castilleja <castilleja.miguel@gmail.com>
2023-04-19 20:45:54 -0600

    Implementado Cors

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const colors = require('colors')
 const dotenv = require('dotenv').config()
 const { errorHandle } = require('./middleware/errorMiddleware')
+const cors = require('cors')
 const connectDB = require('./config/bd')
 const port = process.env.PORT || 5000
 
@@ -9,6 +10,7 @@ connectDB()
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
